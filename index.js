bc14409fea45b7d7b20d797ecb302f98a9f3d05b
SunilBaghel002 <sunilbaghel93100@gmail.com>
2025-03-08 18:32:18 +0530

    again

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  const data = fs.readFileSync("index.html");
-  res.send(data.toString());
+  res.sendFile(path.join(__dirname, "index.html"));
 });
+
 app.get("/about", (req, res) => {
-  const data = fs.readFileSync("about.html");
-  res.send(data.toString());
+  res.sendFile(path.join(__dirname, "about.html"));
 });
+
 app.get("/service", (req, res) => {
-  const data = fs.readFileSync("services.html");
-  res.send(data.toString());
+  res.sendFile(path.join(__dirname, "services.html"));
 });
+
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "notfound.html"));
 });
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
